6e6272f953cb8ad3447a0cbca9275d01370292d0
Nico Orschel <nico.orschel@xebia.com>
2025-12-05 12:34:30 +0100

    Refactor network request tests: Rename methods for clarity and enhance routing logic

--- PlaywrightDemos/PlaywrightE2ETests_IT_Tage_2025.cs
+++ PlaywrightDemos/PlaywrightE2ETests_IT_Tage_2025.cs
@@ -252,8 +252,9 @@ public class PlaywrightE2ETests_IT_Tage_2025
     #endregion
 
     #region Network Requests
+
     [TestMethod]
-    public async Task ITT_NetworkRequestTest()
+    public async Task ITT_NetworkRequest_Simple_Test()
     {
         var playwright = await Playwright.CreateAsync();
         await using var browser = await playwright.Chromium.LaunchAsync(
@@ -265,12 +266,48 @@ public class PlaywrightE2ETests_IT_Tage_2025
         var browserContext = await browser.NewContextAsync();
         var page = await browserContext.NewPageAsync();
 
-        //await page.RouteAsync("**/*.jpg", route => route.FulfillAsync(new()
-        //{
-        //    Status = 404,
-        //    ContentType = "text/plain",
-        //    Body = "Not Found!"
-        //}));
+        await page.RouteAsync("**/*.jpg", route => route.FulfillAsync(new()
+        {
+            Status = 404,
+            ContentType = "text/plain",
+            Body = "Not Found!"
+        }));
+
+        await page.GotoAsync("https://www.ittage.informatik-aktuell.de/");
+        await page.GotoAsync("https://www.ittage.informatik-aktuell.de/programm.html");
+
+        await page.Locator("[href*=playwright]").ScrollIntoViewIfNeededAsync();
+        await page.Locator("[href*=playwright]").HighlightAsync();
+        // use it only for testing or debugging
+        //await page.PauseAsync();
+
+        await page.Locator("[href*=playwright]").ClickAsync();
+
+        await page.ScreenshotAsync(new PageScreenshotOptions { Path = "session_before.png" });
+
+        await page.ScreenshotAsync(new PageScreenshotOptions { Path = "session_before.png" });
+        //await page.PauseAsync();
+
+        Assert.IsTrue(
+            await page.GetByText("Harald Binkle").First.IsVisibleAsync() &
+            await page.GetByText("Nico Orschel").First.IsVisibleAsync());
+
+        //await page.PauseAsync();
+        await browser.CloseAsync();
+    }
+
+    [TestMethod]
+    public async Task ITT_NetworkRequest_FullTest()
+    {
+        var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(
+            new BrowserTypeLaunchOptions
+            {
+                Headless = _isHeadless,
+                SlowMo = 2000,
+            });
+        var browserContext = await browser.NewContextAsync();
+        var page = await browserContext.NewPageAsync();
 
         //https://www.ittage.informatik-aktuell.de/fileadmin…ocessed_/e/3/csm_300-Harald-Binkle_4245c411b0.jpg
         await page.RouteAsync("**/*Harald-Binkle*.jpg", async route =>
@@ -279,7 +316,7 @@ public class PlaywrightE2ETests_IT_Tage_2025
 
                 var body = await response.BodyAsync();
                 // read local image and replace the response body
-                
+
                 var assemblyDir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) ?? AppContext.BaseDirectory;
                 var filePath = Path.Combine(assemblyDir, "testdaten", "santa_harry.jpg");
                 body = await File.ReadAllBytesAsync(filePath);
@@ -295,27 +332,27 @@ public class PlaywrightE2ETests_IT_Tage_2025
                 });
             });
 
-            await page.RouteAsync("**/*Nico-Orschel*.jpg", async route =>
+        await page.RouteAsync("**/*Nico-Orschel*.jpg", async route =>
+        {
+            var response = await route.FetchAsync();
+
+            var body = await response.BodyAsync();
+            // read local image and replace the response body
+
+            var assemblyDir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) ?? AppContext.BaseDirectory;
+            var filePath = Path.Combine(assemblyDir, "testdaten", "santa_nico.jpg");
+            body = await File.ReadAllBytesAsync(filePath);
+
+            await route.FulfillAsync(new RouteFulfillOptions
             {
-                var response = await route.FetchAsync();
-
-                var body = await response.BodyAsync();
-                // read local image and replace the response body
-                
-                var assemblyDir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) ?? AppContext.BaseDirectory;
-                var filePath = Path.Combine(assemblyDir, "testdaten", "santa_nico.jpg");
-                body = await File.ReadAllBytesAsync(filePath);
-
-                await route.FulfillAsync(new RouteFulfillOptions
+                Response = response,
+                BodyBytes = body,
+                Headers = new Dictionary<string, string>(response.Headers)
                 {
-                    Response = response,
-                    BodyBytes = body,
-                    Headers = new Dictionary<string, string>(response.Headers)
-                    {
-                        ["Content-Type"] = "application/image-jpeg",
-                    }
-                });
+                    ["Content-Type"] = "application/image-jpeg",
+                }
             });
+        });
 
 
         await page.GotoAsync("https://www.ittage.informatik-aktuell.de/");
@@ -331,7 +368,7 @@ public class PlaywrightE2ETests_IT_Tage_2025
         await page.ScreenshotAsync(new PageScreenshotOptions { Path = "session_before.png" });
 
         await page.ScreenshotAsync(new PageScreenshotOptions { Path = "session_before.png" });
-        await page.PauseAsync();
+        //await page.PauseAsync();
 
         Assert.IsTrue(
             await page.GetByText("Harald Binkle").First.IsVisibleAsync() &
